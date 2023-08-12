de4d0388e7d2e7d41fe29e744339c73adc51cf5e
DioonS <itzdiih@gmail.com>
2023-08-12 19:08:59 -0300

    Escopo de instancias de serviços e módulos

--- curso-angular2/servicos/src/app/cursos/cursos.component.ts
+++ curso-angular2/servicos/src/app/cursos/cursos.component.ts
@@ -5,16 +5,18 @@ import { CursosService } from './cursos.service';
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  styleUrls: ['./cursos.component.css'],
+  providers: [CursosService]
 })
 export class CursosComponent {
 
   cursos: string[] = [];
 
-  cursosService: CursosService;
+  //cursosService: CursosService;
 
-  constructor() {
-    this.cursosService = new CursosService();
+  constructor(private cursosService: CursosService) {
+    //this.cursosService = new CursosService();
+    //this.cursosService = cursosService;
   }
 
   ngOnInit() {
