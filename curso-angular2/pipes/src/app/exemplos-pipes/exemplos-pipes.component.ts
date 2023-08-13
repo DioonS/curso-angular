fad861ef39bc21dc772e3f950d791989af3696bf
DioonS <itzdiih@gmail.com>
2023-08-13 16:43:26 -0300

    Usando pipes, parâmetros e pipes aninhados

---
+++ curso-angular2/pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.css']
+})
+export class ExemplosPipesComponent {
+
+  // Criando o objeto livro
+  livro: any = {
+    title: 'Learning JavaScript Data Structures and Algorithms: Hone your skills by learning classic data structures and algorithms in JavaScript, 2nd Edition',
+    rating: 4.54321, 
+    numeroPaginas: 314,
+    preco: 46.99,
+    dataLancamento: new Date(2016, 6, 23),
+    url: 'https://a.co/glqjpRP'
+  };
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+}
