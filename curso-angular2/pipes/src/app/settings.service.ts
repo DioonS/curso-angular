39894cbb1dc02cfe4516aa31f909afedabd745a0
DioonS <itzdiih@gmail.com>
2023-08-13 18:36:13 -0300

    Aplicando Locale (internacionalização) nos pipes

---
+++ curso-angular2/pipes/src/app/settings.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingsService {
+  static getLocale() {
+    throw new Error('Method not implemented.');
+  }
+
+  constructor() { }
+
+  getLocale() {
+    return 'pt-BR';
+  }
+}
