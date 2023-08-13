936a3deed8bb31332c71b55632fb8b553eec529d
DioonS <itzdiih@gmail.com>
2023-08-13 16:00:08 -0300

    Injetando um serviço em outro serviço

--- curso-angular2/servicos/src/app/app.module.ts
+++ curso-angular2/servicos/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { CursosModule } from './cursos/cursos.module';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,8 @@ import { CursosModule } from './cursos/cursos.module';
     CriarCursoModule,
     CursosModule
   ],
-  //providers: [CursosService],
+  providers: [LogService], // Escopo global do service
+  //providers: [LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
