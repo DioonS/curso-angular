b976619a9d77cd00df42f0265b2d6f03a537ba29
DioonS <itzdiih@gmail.com>
2023-08-23 17:14:57 -0300

    Rotas imperativas - Redirecionamento via código

--- curso-angular2/rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ curso-angular2/rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { CursosService } from '../cursos/cursos.service';
 
 @Component({
   selector: 'app-curso-detalhe',
   templateUrl: './curso-detalhe.component.html',
   styleUrls: ['./curso-detalhe.component.css']
 })
-export class CursoDetalheComponent {
+export class CursoDetalheComponent implements OnInit, OnDestroy {
 
-  id: string | undefined;
+  id!: number;
 
-  constructor(private route: ActivatedRoute) {
-    this.id = this.route.snapshot.params['id'];
+  inscricao: Subscription = new Subscription;
+
+  curso: any;
+
+  constructor(private route: ActivatedRoute, private cursosService: CursosService, private router: Router) {
+    //this.id = this.route.snapshot.params['id'];
+    //console.log(this.route);
+  }
+
+  ngOnInit() {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+        this.curso = this.cursosService.getCurso(this.id);
+
+        if (this.curso == null) {
+          this.router.navigate(['/naoEncontrado']);
+        }
+    });
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
   }
 }
+
