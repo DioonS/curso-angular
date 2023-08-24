ffa19e9059cecf5aa0e39529476dbbb2aebf033f
DioonS <itzdiih@gmail.com>
2023-08-24 08:28:30 -0300

    Refatoring

--- curso-angular2/rotas/src/app/app.module.ts
+++ curso-angular2/rotas/src/app/app.module.ts
@@ -5,26 +5,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
-//import { routing } from './app.routing';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CursosComponent,
     LoginComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    //routing
+    CursosModule,
+    AppRoutingModule
   ],
   providers: [CursosService],
   bootstrap: [AppComponent]
